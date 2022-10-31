1545fb7e349a8235b1f803e1abefc325019b04ad
Rijul Saxena <rijul.saxena@gmail.com>
2022-10-31 12:08:32 +0530

    Update ARIMA_model.py
    updated code with pickling

--- arimasarima/ARIMA_model.py
+++ arimasarima/ARIMA_model.py
@@ -1,5 +1,6 @@
 from modules import *
 
+
 def ARIMA_model_fit(train_data, test_data):
     model_predictions = []
     training_data = train_data['Close'].values
@@ -18,5 +19,10 @@ def ARIMA_model_fit(train_data, test_data):
 
     MSE_error = mean_squared_error(test_data, model_predictions)
     print('Testing Mean Squared Error is {}'.format(MSE_error))
-    
+
+    try:
+        with open("Models/Arima_"+str(int(math.sqrt(MSE_error)))+".pkl", 'wb') as f:
+            pickle.dump(model, f)
+    except Exception as e:
+        print(e)
     return model_predictions
